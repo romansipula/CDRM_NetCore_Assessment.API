16434fb3377f7b7f8bc690787c4c78556a84405e
romansipula <romansipula@gmail.com>
2025-05-08 16:53:40 +0200

    Add files via upload

---
+++ CDRM_NetCore_Assessment.API/Program.cs
@@ -0,0 +1,34 @@
+using CDRM_NetCore_Assessment.API.Storage;
+using CDRM_NetCore_Assessment.API.Formatters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register storage
+builder.Services.AddSingleton<IDocumentStorage, InMemoryDocumentStorage>();
+
+// Register formatters
+builder.Services.AddSingleton<IDocumentFormatter, JsonDocumentFormatter>();
+builder.Services.AddSingleton<IDocumentFormatter, XmlDocumentFormatter>();
+builder.Services.AddSingleton<IDocumentFormatter, MessagePackDocumentFormatter>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
---
+++ CDRM_NetCore_Assessment.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CDRM_NetCore_Assessment.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
